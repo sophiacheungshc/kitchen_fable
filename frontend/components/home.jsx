9437ea926a76e98340411233838e375727b15602
Sophia Cheung <cheungsinhan@gmail.com>
2019-07-10 16:42:15 -0700

    Enable search by city by clicking on featured areas

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -1,42 +1,64 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import SearchContainer from './search_container';
+import { connect } from 'react-redux';
+import { searchRestaurants } from '../actions/restaurant_actions';
 
-function Home() { 
+const mDP = (dispatch) => ({
+    searchRestaurants: keyword => dispatch(searchRestaurants(keyword))
+});
 
-    return(
-        <>
-            <div className='carousel'>
-                <h1>Find your table for any occasion</h1>
-                <div className='slide1'></div>
-                <div className='slide2'></div>
-                <div className='slide3'></div>
-                <SearchContainer />
-            </div>
-            <div className="featured-areas">
-                <div className="ny">
-                    <span className="area-name">New York Area</span>
+class Home extends React.Component { 
+    constructor(props){
+        super(props);
+
+        this.searchFeatured = this.searchFeatured.bind(this);
+    }
+
+    searchFeatured(keyword){
+        this.props.searchRestaurants(keyword)
+            .then(() => this.props.history.push({
+                pathname: '/restaurants',
+                search: `?search=${keyword}`
+            })); 
+    }
+
+    render(){
+        return(
+            <>
+                <div className='carousel'>
+                    <h1>Find your table for any occasion</h1>
+                    <div className='slide1'></div>
+                    <div className='slide2'></div>
+                    <div className='slide3'></div>
+                    <SearchContainer />
                 </div>
-                <div className="chicago">
-                    <span className="area-name">Chicago</span>
+                <h3 className="featured-head">Featured Areas</h3>
+                <div className="featured-areas">
+                    <div className="ny" onClick={() => this.searchFeatured('new york')}>
+                        <span className="area-name">New York Area</span>
+                    </div>
+                    <div className="chicago" onClick={() => this.searchFeatured('chicago')}>
+                        <span className="area-name">Chicago</span>
+                    </div>
+                    <div className="la" onClick={() => this.searchFeatured('los angeles')}>
+                        <span className="area-name">Los Angeles</span>
+                    </div>
+                    <div className="sf" onClick={() => this.searchFeatured('san fran')}>
+                        <span className="area-name">San Francisco</span>
+                    </div>
+                    <div className="sd" onClick={() => this.searchFeatured('san diego')}>
+                        <span className="area-name">San Diego</span>
+                    </div>
+                    <div className="lv" onClick={() => this.searchFeatured('las vegas')}>
+                        <span className="area-name">Las Vegas</span>
+                    </div>
                 </div>
-                <div className="la">
-                    <span className="area-name">Los Angeles</span>
-                </div>
-                <div className="sf">
-                    <span className="area-name">San Francisco</span>
-                </div>
-                <div className="sd">
-                    <span className="area-name">San Diego</span>
-                </div>
-                <div className="lv">
-                    <span className="area-name">Las Vegas</span>
-                </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
 
 };
 
 
-export default Home;
+export default withRouter(connect(null, mDP)(Home));
